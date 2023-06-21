c13093bf1bec20d88c4928f17ab7d6138250f61c
chiteshbansal <63744720+chiteshbansal@users.noreply.github.com>
2023-06-21 17:21:57 +0530

    Improved error handling and naming in ratelimiter

--- internal/middlewares/rateLimiter.go
+++ internal/middlewares/rateLimiter.go
@@ -11,18 +11,32 @@ import (
 
 func RateLimitMiddleware(c *gin.Context, redisClient cache.UserCache, tb *ratelimiter.TokenBucket) {
 	// Check if the Token Bucket has enough tokens
-	if tb.Take(c, redisClient) {
+	if tb.IsTotalRequestAllowed(redisClient) {
 		// Perform the request
 		// check for per client basis request
-		clientID :=GetClientIdentity(c)
-		clientTB := ratelimiter.GetClientBucket(clientID, "user",redisClient)
+		clientID := GetClientIdentity(c)
+		clientTB := ratelimiter.GetClientBucket(clientID, "user", redisClient)
+		if clientTB == nil {
+			fmt.Println("failed to retreive client bucket from redis server")
+			c.AbortWithStatusJSON(http.StatusBadGateway, gin.H{
+				"message": "Something went wrong try again later ",
+			})
+			return
+		}
 		fmt.Println("clientID:", clientID)
-		if !clientTB.IsRequestAllowed(1) {
-			fmt.Println("too many request by ",clientID)
+		if !clientTB.IsClientRequestAllowed(1) {
+			fmt.Println("too many request by ", clientID)
 			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
 				"message": "Too many requests , try again after sometimes!",
 			})
 			return
+		}
+		err := ratelimiter.UpdateClientBucketInRedis(redisClient)
+		if err != nil {
+			fmt.Println("Error:", err)
+			c.AbortWithStatusJSON(http.StatusBadGateway, gin.H{
+				"message": err,
+			})
 		}
 		c.Next()
 	} else {
@@ -34,6 +48,7 @@ func RateLimitMiddleware(c *gin.Context, redisClient cache.UserCache, tb *rateli
 func GetClientIdentity(c *gin.Context) string {
 	ip := c.ClientIP()
 	url := c.Request.URL.Path
-	data := fmt.Sprintf("%s-%s", ip, url)
+	method :=c.Request.Method
+	data := fmt.Sprintf("%s-%s-%s", ip, url,method)
 	return data
 }
