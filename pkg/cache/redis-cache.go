ec21b1510f8072a54bc0d2bf46832c2289b58468
chiteshbansal <63744720+chiteshbansal@users.noreply.github.com>
2023-06-21 15:17:49 +0530

    Changed the redis library from go-redis to redigo to implement redsync which uses redigo interface

--- pkg/cache/redis-cache.go
+++ pkg/cache/redis-cache.go
@@ -1,72 +1,68 @@
-// Package cache provides functionalities to interact with a Redis cache.
 package cache
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"time"
 
-	"github.com/redis/go-redis/v9"
+	"github.com/gomodule/redigo/redis"
 )
 
 // redisCache is a struct that holds the Redis host, database, and expiration details.
 type redisCache struct {
-	host    string
-	db      int
+	pool    *redis.Pool
 	expires time.Duration
 }
 
 // NewRedisCache returns a new instance of redisCache.
 func NewRedisCache(host string, db int, exp time.Duration) *redisCache {
+	pool := &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.Dial("tcp", host)
+			if err != nil {
+				return nil, err
+			}
+			if db != 0 {
+				_, err := conn.Do("SELECT", db)
+				if err != nil {
+					conn.Close()
+					return nil, err
+				}
+			}
+			return conn, nil
+		},
+	}
+
 	return &redisCache{
-		host:    host,
-		db:      db,
+		pool:    pool,
 		expires: exp,
 	}
 }
 
-// getClient establishes a new connection with the Redis client.
-func (cache *redisCache) getClient() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     cache.host,
-		Password: "",
-		DB:       cache.db,
-	})
-
-	ctx := context.Background()
-	pong, err := client.Ping(ctx).Result()
-
-	// If an error occurred while pinging, log it and proceed.
-	if err != nil {
-		fmt.Println("Error pinging Redis server:", err)
-	}
-	// Print the response from the Redis server.
-	fmt.Println(pong)
-
-	return client
-}
-
 // Set stores the object in the Redis cache against the provided key.
 func (cache *redisCache) Set(key string, value interface{}, exp *time.Duration) error {
-	fmt.Println("Caching the query: ", key, value)
+	fmt.Println("Caching the query:", key, value)
 
-	// Obtain the Redis client.
-	client := cache.getClient()
+	// Obtain a connection from the Redis pool.
+	conn := cache.pool.Get()
+	defer conn.Close()
 
 	// Marshal object into JSON.
-	json, err := json.Marshal(value)
+	jsonData, err := json.Marshal(value)
 	if err != nil {
 		return err
 	}
 
-	ctx := context.Background()
-
 	// Set the key-value pair in the Redis cache with the expiry duration.
 	if exp == nil {
-		err = client.Set(ctx, key, json, cache.expires*time.Second).Err()
+		fmt.Println(cache.expires*time.Second, cache.expires, (cache.expires.Seconds()))
+		expiration := cache.expires * time.Second
+		_, err = conn.Do("SETEX", key, expiration.Seconds(), jsonData)
 	} else {
-		err = client.Set(ctx, key, json, *exp).Err()
+		fmt.Println((exp.Seconds()))
+		_, err = conn.Do("SETEX", key, int(exp.Seconds()), jsonData)
 	}
 
 	if err != nil {
@@ -78,20 +74,19 @@ func (cache *redisCache) Set(key string, value interface{}, exp *time.Duration) 
 
 // Get retrieves the object from the Redis cache for the given key.
 func (cache *redisCache) Get(key string) (value interface{}, err error) {
-	// Obtain the Redis client.
-	client := cache.getClient()
-
-	ctx := context.Background()
+	// Obtain a connection from the Redis pool.
+	conn := cache.pool.Get()
+	defer conn.Close()
 
 	// Get the value for the given key.
-	val, err := client.Get(ctx, key).Result()
-	if err != nil || val == "" {
+	val, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil || val == nil {
 		// Return nil if an error occurred or if the key does not exist.
 		return nil, err
 	}
 
 	// Unmarshal the JSON value into the interface{}.
-	err = json.Unmarshal([]byte(val), &value)
+	err = json.Unmarshal(val, &value)
 	if err != nil {
 		return nil, err
 	}
