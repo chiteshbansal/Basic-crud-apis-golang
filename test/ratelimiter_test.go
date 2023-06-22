44a09535a90037a489a5dbe8cf56880f46ffb969
chiteshbansal <63744720+chiteshbansal@users.noreply.github.com>
2023-06-22 16:47:23 +0530

    "Fixed a bug in the ratelimiter test"

--- test/ratelimiter_test.go
+++ test/ratelimiter_test.go
@@ -12,41 +12,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
-
-// RateLimiterCache is a mock implementation of the cache.UserCache interface.
-// It is used for testing the rate limiter middleware.
-type RateLimiterCache struct {
-	cache.UserCache
-	mock.Mock
-}
-
-// Set is a mock implementation of the cache.UserCache Set method.
-func (m *RateLimiterCache) Set(key string, value interface{}, exp *time.Duration) error {
-	args := m.Called(key, value, exp)
-	return args.Error(0)
-}
-
-// Get is a mock implementation of the cache.UserCache Get method.
-func (m *RateLimiterCache) Get(key string) (interface{}, error) {
-	args := m.Called(key)
-	return args.Get(0), args.Error(1)
-}
 
 // TestTokenBucketMiddleware is a test function to verify the behavior of the rate limiter middleware.
 func TestTokenBucketMiddleware(t *testing.T) {
-	// Create a test Redis client
-	rateLimiterCache := &RateLimiterCache{}
+	rateLimiterCache := cache.NewRedisCache("localhost:6379", 1, 1000)
 
 	// Create a new TokenBucket and set the initial token count in the cache
 
 	tb := ratelimiter.NewTokenBucket(100, 1, time.Now())
 
-	// Set up the mock expectations for the cache calls
-	rateLimiterCache.On("Set", "token_bucket", 99, mock.AnythingOfType("*time.Duration")).Return(nil)
-	rateLimiterCache.On("Get", "token_bucket").Return(tb.Limit+1, nil)
-	
 	// Create a new Gin router and apply the middleware
 	router := gin.New()
 	router.Use(func(ctx *gin.Context) {
@@ -60,12 +35,10 @@ func TestTokenBucketMiddleware(t *testing.T) {
 	})
 
 	// Perform test requests
-	for i := 1; i <= 500; i++ {
+	for i := 1; i <= 5; i++ {
 		req, _ := http.NewRequest("GET", "/test", nil)
 		resp := httptest.NewRecorder()
-
 		router.ServeHTTP(resp, req)
-		fmt.Println("Sending request no:", i)
 
 		// Check if the response code matches the expected status code
 		if resp.Code != http.StatusOK {
